1d1351a93721d2afdb9488ba8d4737eeec94ee46
Alexey Min <alexey.min@gmail.com>
2016-02-20 00:25:03 +0500

    Add pass field and remember checkbox. save/restore activity state...

--- app/src/main/java/ru/minlexx/xnovaalarm/MainActivity.java
+++ app/src/main/java/ru/minlexx/xnovaalarm/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
 
 
 public class MainActivity extends Activity {
@@ -17,8 +20,36 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState()");
+        //
+        final CheckBox cb = (CheckBox)findViewById(R.id.cb_remember);
+        final EditText e_login = (EditText)findViewById(R.id.et_xnovalogin);
+        final EditText e_pass = (EditText)findViewById(R.id.et_xnovapassword);
+        //
+        outState.putBoolean("cb_remember", cb.isChecked());
+        outState.putString("login", e_login.getText().toString());
+        outState.putString("pass", e_pass.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState()");
+        //
+        final CheckBox cb = (CheckBox)findViewById(R.id.cb_remember);
+        final EditText e_login = (EditText)findViewById(R.id.et_xnovalogin);
+        final EditText e_pass = (EditText)findViewById(R.id.et_xnovapassword);
+        //
+        cb.setChecked(savedInstanceState.getBoolean("cb_remember"));
+        e_login.setText(savedInstanceState.getString("login"));
+        e_pass.setText(savedInstanceState.getString("pass"));
+    }
+
     public void onClickStartService(View view) {
-        Log.d(TAG, "onClickStartService()");
+        //Log.d(TAG, "onClickStartService()");
         Intent ssi = new Intent(this, RefresherService.class);
         startService(ssi);
 
@@ -26,7 +57,7 @@ public class MainActivity extends Activity {
     }
 
     public void onClickStopService(View view) {
-        Log.d(TAG, "onClickStopService()");
+        //Log.d(TAG, "onClickStopService()");
         Intent ssi = new Intent(this, RefresherService.class);
         stopService(ssi);
     }
